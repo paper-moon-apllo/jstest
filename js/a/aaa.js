5c2a6c7e46a1fb12cfc1b85d075d6a94719c6a77
nomura <test@test.com>
2017-11-17 13:36:55 +0900

    init js test

---
+++ js/a/aaa.js
@@ -0,0 +1,3 @@
+function sum(val1, val2){
+    return val1 + val2;
+}
---
+++ test/b/aaa.js
@@ -0,0 +1,11 @@
+describe("sum関数のテスト", function(){
+    it("1+1=2", function(){
+        expect(sum(1, 1)).toBe(2);
+    });
+    it("-1+1=0", function(){
+        expect(sum(-1, 1)).toBe(0);
+    });
+    it("0+1=1", function(){
+        expect(sum(0, 1)).toBe(1); //ミスするテスト
+    });
+});
